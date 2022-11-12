f7775a382fbdf858023233ea0bb9f3c705e73fd4
Jin <darkbomb1234@outlook.com>
2022-11-12 05:48:17 +0100

    first commit

---
+++ utils/constants.js
@@ -0,0 +1,18 @@
+export const API_ROUTES = {
+  SIGN_UP: '/api/auth/signup',
+  SIGN_IN: '/api/auth/signin',
+  SIGN_OUT: '/api/auth/signout',
+  GET_USER: '/api/auth/getuser',
+  SOCKETIO: '/api/socketio',
+};
+
+export const APP_ROUTES = {
+  SIGN_UP: '/signup',
+  SIGN_IN: '/signin',
+  HOME: '/',
+  PROFILE: '/profile',
+  CRYPTO_RANKING: '/ranking',
+};
+
+export const CRYPTO_API_CALL_PERIOD = 3000;
+export const CRYPTO_API_TOKEN_COUNT = 10;
